3ac6da7db43f38acc9190fd32a1985671f43a428
NeerajKumar3196 <neeraj.kumar@collabera.com>
2022-06-24 18:59:16 +0530

    completed day_1 with strings and variables

---
+++ day_1/main.py
@@ -0,0 +1,15 @@
+# ============ input function on print statement =========
+# input() will get user input on console
+
+
+print ("hello "+ input("what is you name?")+ "!")
+
+
+
+#===============
+name ="abc"
+print(name)
+#name="xyz"
+
+
+
---
+++ day_1/project_day_1.py
@@ -0,0 +1,13 @@
+print("------------ Welcome to the band name generator ------------------")
+# ask the usr for city name
+a=input("which city you are grew up?  : \n ")
+
+print("------------------------")
+#ask the user for pet name
+b=input("what's your pet name? : \n")
+
+print("----------------------------")
+# now combine their city name and pet name to show their band name
+print (" hey there your band name is :" + a +" "+b)
+
+print("-------------------------")
